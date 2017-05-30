c60cd0fe17a80b844e56f5a9aea462ec1a8e7330
Ahmed Kamel <ak@ahmedkamel.net>
2017-05-30 14:40:26 -0700

    initial work

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
-var config    = require('config')
+var config    = require('config-url')
   , winston   = require('winston')
+  , redis     = require('redis')
+  , urljoin   = require('url-join')
+  , rp        = require('request-promise')
   ;
 
+Promise.promisifyAll(redis.RedisClient.prototype);
+Promise.promisifyAll(redis.Multi.prototype);
+
+var client = redis.createClient(config.get('make.redis'));
+
+let make_url = urljoin(config.getUrl('make'), 'make');
+
 function make(remote, options = {}) {
-  
+  let sha         = options.branch
+    , single_use  = options.single_use
+    , key         = `${remote}#${sha}`
+    ;
+
+  // todo [akamel] single_use
+  if (single_use) {
+    key = `${key}+foo`
+  }
+
+  return client
+           .getAsync(key)
+           .then((run_url) => {
+             if (!run_url) {
+              //  return rp.post(make_url, { json : { remote, sha } });
+              return {};
+             }
+
+             return {
+               run_url
+             };
+           });
 }
 
 module.exports = {
