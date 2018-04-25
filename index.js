79b1ea53f429f28d4fbcdf0de25d139eaf2dfa04
Ahmed Kamel <ak@ahmedkamel.net>
2018-04-24 23:16:47 -0700

    undefined timeout

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ function make(remote, sha, options = {}) {
 }
 
 function make_new(remote, sha, options = {}) {
-  let { blob, filename, token, bearer, cache, tailf, key, hash } = options;
+  let { blob, filename, token, bearer, cache, tailf, key, hash, timeout = 20 * 1000 } = options;
 
   let json = { remote, sha, blob, filename, token, cache, tailf };
 
